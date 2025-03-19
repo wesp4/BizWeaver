71c4d64d8ceab4fd2a4db4119f911359d4837a68
Pavel Ivanchik <wesp416532@gmail.com>
2025-03-19 20:21:52 +0300

    first commit

---
+++ src/main/java/ru/bizweaver/core/BizWeaverApplication.java
@@ -0,0 +1,13 @@
+package ru.bizweaver.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizWeaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizWeaverApplication.class, args);
+	}
+
+}
